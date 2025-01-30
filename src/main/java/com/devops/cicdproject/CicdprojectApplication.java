dbf29ee8b5c62af4a50f298ce558a4dcc11439c7
parmuthu <parmuthu2002@gmail.com>
2025-01-30 11:16:11 +0530

    main class modified

--- src/main/java/com/devops/cicdproject/CicdprojectApplication.java
+++ src/main/java/com/devops/cicdproject/CicdprojectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CicdprojectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Update cicd pipeline");
+		System.out.println("Update cicd pipeline project");
 		SpringApplication.run(CicdprojectApplication.class, args);
 		
 	}
